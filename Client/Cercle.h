3e1aa85298765d30d9d9a5271c6ecb232a412c3c
SamvelMan <manukyan.samvel20@gmail.com>
2022-02-02 11:17:26 +0100

    Ajout Client Cercle.h

---
+++ Client/Cercle.h
@@ -0,0 +1,23 @@
+//
+// Created by Samvel on 02/02/2022.
+//
+
+#ifndef PPIL_MANUKYAN_LAGLER_CERCLE_H
+#define PPIL_MANUKYAN_LAGLER_CERCLE_H
+
+#include "Forme.h"
+
+class Cercle : public Forme{
+    private:
+        Point2D point;
+        double rayon;
+    public:
+        Cercle(const string &nom, const Point2D &point, double rayon);
+        double getRayon() const;
+        void setRayon(double rayon);
+        string toString() const;
+};
+
+    ostream &operator<<(ostream &os, const Cercle &cercle);
+
+#endif //PPIL_MANUKYAN_LAGLER_CERCLE_H
